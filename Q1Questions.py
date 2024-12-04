6755d3860e189a9f97278bfe5e9a90426c0fe67b
Lucas <Lucas.vu@stu.ocsb.ca>
2024-12-04 10:00:09 -0500

    testing

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,8 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
+question = input("Enter the question: ")
+answer_a = input("Enter answer A: ")
+answer_b = input("Enter answer B: ")
+answer_c = input("Enter answer C: ")
+answer_d = input("Enter answer D: ")
+correct_answer = input("Enter the correct answer: ")
